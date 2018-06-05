81a97143bad3d215052057c2faac0f090ba18f9c
Mark Armendariz <src@enobrev.com>
2018-06-05 00:28:32 -0500

    Added simple cmdline test

---
+++ tests/test.js
@@ -0,0 +1,24 @@
+
+const fs        = require('fs');
+const MediaInfo = require('../lib/mediainfo.js');
+
+if (process.argv.length < 3) {
+    console.error('Usage: node test [filename]')
+    process.exit(1);
+}
+
+const sFile = process.argv[2];
+
+if (!fs.existsSync(sFile)) {
+    console.error('File not found');
+    process.exit(1);
+}
+
+MediaInfo(sFile, function(oError, oInfo) {
+    console.log('calling mediainfo for this file', sFile);
+    if (oError) {
+        console.error('MediaInfo #FAIL', oError);
+    } else {
+        console.log('MediaInfo Succeeds!', JSON.stringify(oInfo, null, '    '));
+    }
+});
